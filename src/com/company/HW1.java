cd43bc6f13b0ebb9dea1077d924c58ba1889b1cd
Michael <Mykl2732@aol.com>
2022-05-14 22:46:08 -0400

    First git homework

---
+++ src/com/company/HW1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class HW1 {
+
+        public static void main(String[] args) {
+            for (int i = 0; i < 101; i++) {
+
+                System.out.print(i+",");
+            }
+        }
+}
